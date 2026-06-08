b5f5cbccc9d4ae49ad940c4c8780da594a95d322
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-06-08 11:59:05 -0500

    Added Patrons component

---
+++ src/data/patronsData.js
@@ -0,0 +1,18 @@
+const _apiUrl = "/api/patrons";
+
+export const getPatrons = () => {
+  return fetch(_apiUrl).then((r) => r.json());
+};
+
+//export a function here that gets a patron by id
+export const getPatron = (id) => {
+  return fetch(`${_apiUrl}/${id}`).then((r) => r.json());
+};
+
+export const createPatron = (patron) => {
+  return fetch(_apiUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(patron),
+  }).then((res) => res.json());
+};
